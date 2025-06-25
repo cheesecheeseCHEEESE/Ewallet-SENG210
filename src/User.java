9b7bcc33a53ec66923f28808f2c81ec49d2dbbd4
Amya Watkins <122550418+watkinsamya@users.noreply.github.com>
2025-06-25 11:43:13 -0400

    Add constructor to User.java to initialize username, password, and account fields

--- src/User.java
+++ src/User.java
@@ -11,26 +11,29 @@ public class User {
 	// possible monthly savings, calculated using monthly income (most recent) assuming the data we have is for one year, and monthly and biweekly expenses, here you can assume yearly expenses that are recorded have already been paid. 
 	double monthlySavings;	
 	
-	//should add constructor(s)
-	User(String username, String password)
-	{
-		
-	}
-	public void addExpense(Expense Ex) {
-		spending.add(Ex);
-	}
-	
-	public void addIncome(Wage W) {
-		income.add(W);
-	}
-	
-	
-	public ArrayList<Wage> getIncome() {
-		return income;
-	}
-	
-	public ArrayList<Expense> getSpending() {
-		return spending;
-	}
-	
+	public User(String username, String password) {
+        this.username = username;
+        this.pwd = password;
+        this.income = new ArrayList<>();
+        this.spending = new ArrayList<>();
+        this.currencyRates = new ArrayList<>();
+        this.balance = 0.0;
+        this.monthlySavings = 0.0;
+    }
+
+    public void addExpense(Expense Ex) {
+        spending.add(Ex);
+    }
+
+    public void addIncome(Wage W) {
+        income.add(W);
+    }
+
+    public ArrayList<Wage> getIncome() {
+        return income;
+    }
+
+    public ArrayList<Expense> getSpending() {
+        return spending;
+    }
 }
