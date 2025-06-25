1b5d1f846090481f4e02ce0a7403d64308aa4a34
Amya Watkins <122550418+watkinsamya@users.noreply.github.com>
2025-06-25 11:25:49 -0400

    Update Wage.java

--- src/Wage.java
+++ src/Wage.java
@@ -1,8 +1,31 @@
 
 public class Wage {
-	String source;
-	double amount;
-	String month;
-	
-	//should add contructor(s)
+    private double hourlyRate;
+    private int hoursWorked;
+
+    public Wage(double hourlyRate, int hoursWorked) {
+        this.hourlyRate = hourlyRate;
+        this.hoursWorked = hoursWorked;
+    }
+
+    public double getHourlyRate() {
+        return hourlyRate;
+    }
+
+    public void setHourlyRate(double hourlyRate) {
+        this.hourlyRate = hourlyRate;
+    }
+
+    public int getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public void setHoursWorked(int hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+
+    public double calculateWage() {
+        return hourlyRate * hoursWorked;
+    }
 }
+
